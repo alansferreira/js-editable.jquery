7890499d2a85b66e41b0a80555749edc46228163
Alan <alan.da.silva.ferreira@hotmail.com>
2017-05-13 23:41:26 -0300

    #fix gulp task and version

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,15 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var cleancss = require('gulp-clean-css');
 var rename = require('gulp-rename');
+var bump = require('gulp-bump');
+
+// Defined method of updating: 
+// Semantic 
+gulp.task('bump', function(){
+  gulp.src('./*.json')
+  .pipe(bump({type:'patch'}))
+  .pipe(gulp.dest('./'));
+});
 
 gulp.task('compress-js', function () {
   // returns a Node.js stream, but no handling of error messages
@@ -20,4 +29,4 @@ gulp.task('compress-css', function () {
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['compress-css', 'compress-js']);
+gulp.task('default', ['bump', 'compress-css', 'compress-js']);
