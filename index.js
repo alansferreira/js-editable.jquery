cdaa4c75385102b2de8568802eed659aaf39d265
Alan <alan.da.silva.ferreira@hotmail.com>
2017-05-18 18:55:08 -0300

    # fix jquery dependency

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 try {
     
     if(!(require && module && module.exports)) return null;
+    if(!(window && window.$ && window.jQuery)) window.$ = window.jQuery = require('jquery');
     
     module.exports = require('./js-editable.jquery');
     
